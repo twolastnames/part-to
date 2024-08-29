d20533503806ec7b1676b194d1afe35bb49d7cb0
Alan Glenn <alanglenn16@outlook.com>
2024-08-28 19:22:59 -0500

    have blank page hello world

---
+++ project/parttofe/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ParttofeConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "parttofe"
---
+++ project/parttofe/views.py
@@ -0,0 +1,4 @@
+from django.shortcuts import render
+
+def index(request):
+  return render(request, "entry/index.html")
