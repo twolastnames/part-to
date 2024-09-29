b8483881da7c4475ff3c28ae7121c707122fee07
Alan Glenn <alanglenn16@outlook.com>
2024-09-29 13:10:21 -0500

    have formatted shell provider

--- project/parttofe/partto/src/ShellProvider.tsx
+++ project/parttofe/partto/src/ShellProvider.tsx
@@ -1,8 +1,9 @@
-import React from 'react'
+import React from "react";
 
 import { MantineProvider } from "@mantine/core";
-import '@mantine/core/styles.layer.css';
-import { PropsWithChildren } from 'react';
+import "@mantine/core/styles.layer.css";
+import { PropsWithChildren } from "react";
 
-export const ShellProvider = (
-    {children}: PropsWithChildren) => <MantineProvider>{children}</MantineProvider>
+export const ShellProvider = ({ children }: PropsWithChildren) => (
+  <MantineProvider>{children}</MantineProvider>
+);
