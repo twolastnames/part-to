fc4b46fdeae5ba2933c6d00da3297d29ae1cd5ca
Alan Glenn <alanglenn16@outlook.com>
2024-09-29 16:53:23 -0500

    test button click

---
+++ project/parttofe/partto/src/components/Button/test/Click.test.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { describe, expect, test } from "@jest/globals";
+import { fireEvent, render, screen } from "@testing-library/react";
+import { Button } from "../Button";
+import { MantineProvider } from "@mantine/core";
+import { Next } from "../../Icon/Icon";
+
+describe("Button", () => {
+  beforeAll(() => {});
+
+  test("snapshot", async () => {
+    Object.defineProperty(window, "matchMedia", {
+      writable: true,
+      value: jest.fn().mockImplementation((query) => ({
+        matches: false,
+        media: query,
+        onchange: null,
+        addListener: jest.fn(), // Deprecated
+        removeListener: jest.fn(), // Deprecated
+        addEventListener: jest.fn(),
+        removeEventListener: jest.fn(),
+        dispatchEvent: jest.fn(),
+      })),
+    });
+    const onClick = jest.fn();
+    render(
+      <MantineProvider>
+        <Button icon={Next} onClick={onClick}>
+          Hello
+        </Button>
+      </MantineProvider>,
+    );
+    const button = screen.getByTestId("Button");
+    await fireEvent.click(button);
+    expect(onClick).toBeCalledTimes(1);
+  });
+});
