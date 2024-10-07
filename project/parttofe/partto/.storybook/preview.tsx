7de8a059a296b693468877839b48826bfbc93598
Alan Glenn <alanglenn16@outlook.com>
2024-10-07 06:47:42 -0500

    remove storybook padding because this consumes the whole page

--- project/parttofe/partto/.storybook/preview.tsx
+++ project/parttofe/partto/.storybook/preview.tsx
@@ -3,7 +3,9 @@ import React from 'react';
  
 import type { Preview } from '@storybook/react';
 import { MemoryRouter } from "react-router";
- 
+
+export const parameters = { layout: 'fullscreen' }
+
 export const preview: Preview = {
   decorators:[
     (Story) => <MemoryRouter initialEntries={['/']}>
