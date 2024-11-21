006c1430fb565df7027e59a780ba7a3c0888cd48
Alan Glenn <alanglenn16@outlook.com>
2024-11-20 21:04:13 -0600

    have buggy full chain

--- project/parttobe/views/runcompletepost.py
+++ project/parttobe/views/runcompletepost.py
@@ -1,3 +1,26 @@
+from parttobe import models
+from parttobe.views.helpers import handle_run_state
+
+
 def handle(arguments):
-    """TODO: return body here"""
-    return {}
+    unstarted = []
+    for definition in arguments.definitions:
+        for state in arguments.runState.task_states():
+            if state.task != definition:
+                continue
+            if state.operation != models.RunState.Operation.STARTED:
+                unstarted.append(
+                    "task {} is not in a started state".format(
+                        definition.uuid
+                    )
+                )
+    if len(unstarted) > 0:
+        return arguments.respond_400(unstarted)
+
+    return arguments.respond_200(
+        handle_run_state(
+            models.RunState.Operation.COMPLETED,
+            [definition for definition in arguments.definitions],
+            arguments.runState,
+        )
+    )
---
+++ project/parttobe/views/tests_runcompletepost.py
@@ -0,0 +1,81 @@
+import parttobe.views.test_helpers as helpers
+from django.test import TestCase
+from django.test import Client
+import json
+
+
+class RunCompleteTestClass(TestCase):
+    def setUp(self):
+        self.ids = helpers.loadExamples()
+
+    def test_will_400_unstarted_tasks(self):
+        client = Client()
+        data = json.dumps({"partTos": [self.ids[2]]})
+        response = client.post(
+            "/api/run/stage", data, content_type="*"
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue("runState" in response.data)
+        runStateId = response.data["runState"]
+        response = client.get(
+            "/api/partto/?partTo={}".format(self.ids[3])
+        )
+        self.assertEqual(response.status_code, 200)
+        completable = response.data["tasks"][0]
+        complete_body = json.dumps(
+            {
+                "runState": runStateId,
+                "definitions": [completable],
+            }
+        )
+        response = client.post(
+            "/api/run/complete", complete_body, content_type="*"
+        )
+        self.assertEqual(response.status_code, 400)
+
+    def test_can_start_run(self):
+        client = Client()
+        data = json.dumps({"partTos": [self.ids[3]]})
+        response = client.post(
+            "/api/run/stage", data, content_type="*"
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue("runState" in response.data)
+        runStateId = response.data["runState"]
+        state_response = client.get(
+            "/api/run/?runState={}".format(runStateId)
+        )
+        self.assertEqual(response.status_code, 200)
+
+        data = json.dumps(
+            {
+                "runState": runStateId,
+            }
+        )
+        response = client.post(
+            "/api/run/start", data, content_type="*"
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue("runState" in response.data)
+        response = client.get(
+            "/api/run/?runState={}".format(response.data["runState"])
+        )
+        self.assertTrue("started" in response.data)
+        runStateId = response.data["runState"]
+        self.assertEqual(response.status_code, 200)
+        completable = response.data["started"][0]
+        complete_body = json.dumps(
+            {
+                "runState": runStateId,
+                "definitions": [completable],
+            }
+        )
+        response = client.post(
+            "/api/run/complete", complete_body, content_type="*"
+        )
+        self.assertEqual(response.status_code, 200)
+        response = client.get(
+            "/api/run/?runState={}".format(response.data["runState"])
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data["completed"], [completable])
