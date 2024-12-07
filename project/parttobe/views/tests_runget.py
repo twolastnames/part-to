6ed63670e4fa0f4a28a09bd6fd25ebc8fe01035a
Alan Glenn <alanglenn16@outlook.com>
2024-12-07 17:24:43 -0600

    chore: fix updateapi test generation

---
+++ project/parttobe/views/tests_runget.py
@@ -0,0 +1,9 @@
+import parttobe.views.test_helpers as helpers
+from django.test import TestCase
+from django.test import Client
+
+
+class RunGetTestClass(TestCase):
+    """ This is heavily used in other tests.
+        Keeping this here to satisfy api generation"""
+    pass
---
+++ project/parttobe/views/tests_runstartpost.py
@@ -0,0 +1,10 @@
+import parttobe.views.test_helpers as helpers
+from django.test import TestCase
+from django.test import Client
+
+
+class RunstartPostTestClass(TestCase):
+    """ This is heavily used in other tests.
+        Keeping this here to satisfy api generation"""
+    pass
+
