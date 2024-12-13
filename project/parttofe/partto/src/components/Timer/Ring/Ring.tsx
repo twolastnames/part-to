a10d68ed7aad2ac000f216e431cc55a2b07a6f29
Alan Glenn <alanglenn16@outlook.com>
2024-12-13 16:53:55 -0600

    feat: revamp timers

--- project/parttofe/partto/src/components/Timer/Ring/Ring.tsx
+++ project/parttofe/partto/src/components/Timer/Ring/Ring.tsx
@@ -1,15 +1,10 @@
-import React, { useLayoutEffect, useRef, useState } from "react";
+import React from "react";
 
 import classes from "./Ring.module.scss";
 import { RingProps } from "./RingTypes";
 import { RingProgress } from "@mantine/core";
-import { useWindowResize } from "../../../hooks/windowResize";
 
 export function Ring({ magnitude, label }: RingProps) {
-  const { height, width } = useWindowResize();
-  const [size /* TODO: reimplment here */] = useState<number>(1);
-  const ringRef = useRef<HTMLDivElement | null>(null);
-
   const color1 =
     magnitude >= 1 ? "var(--highlight-color)" : "var(--secondary-color)";
   const value1 = magnitude >= 2 ? 1 : magnitude % 1;
@@ -17,28 +12,17 @@ export function Ring({ magnitude, label }: RingProps) {
     magnitude >= 1 ? "var(--secondary-color)" : "var(--highlight-color)";
   const value2 = magnitude >= 2 ? 0 : 1 - (magnitude % 1);
 
-  useLayoutEffect(() => {
-    const currentSize = Math.min(
-      ringRef.current?.offsetHeight || Infinity,
-      ringRef.current?.offsetWidth || Infinity,
-    );
-    if (currentSize !== size) {
-      //TODO: Fix rerendering issue here
-      //setSize(currentSize);
-    }
-  }, [height, width, size]);
-
   return (
-    <div ref={ringRef} className={classes.ring} data-testid="Ring">
+    <div className={classes.ring} data-testid="Ring">
       <RingProgress
-        label={<div className={classes.label}>{label}</div>}
-        size={size}
-        thickness={8}
+        size={100}
+        thickness={100}
         sections={[
           { color: color1, value: value1 * 100 },
           { color: color2, value: value2 * 100 },
         ]}
       />
+      <div className={classes.label}>{label}</div>
     </div>
   );
 }
--- project/parttofe/partto/src/components/Timer/Timer.tsx
+++ project/parttofe/partto/src/components/Timer/Timer.tsx
@@ -4,16 +4,22 @@ import classes from "./Timer.module.scss";
 import { TimerProps } from "./TimerTypes";
 import { Ring } from "./Ring/Ring";
 import { DateTime, getDateTime } from "../../shared/dateTime";
-import { DurationFormat } from "../../shared/duration";
+import { Duration, DurationFormat } from "../../shared/duration";
+
+const getMagnitude = (started: DateTime, duration: Duration) =>
+  (getDateTime().sinceEpoch() - started.sinceEpoch()) /
+  duration.toMilliseconds();
 
 export function Timer({ start, duration }: TimerProps) {
   const [started] = useState<DateTime>(start || getDateTime());
-  const [magnitude, setMagnitude] = useState<number>(0);
+
+  const [magnitude, setMagnitude] = useState<number>(
+    getMagnitude(started, duration),
+  );
 
   useEffect(() => {
     const listener = () => {
-      const passed = getDateTime().sinceEpoch() - started.sinceEpoch();
-      setMagnitude(passed / duration.toMilliseconds());
+      setMagnitude(getMagnitude(started, duration));
     };
     const id = setInterval(listener, 1000);
     return () => {
