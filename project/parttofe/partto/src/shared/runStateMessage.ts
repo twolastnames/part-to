1a39ea92dc24e1f590a287b58cfecd579a96cf9a
Alan Glenn <alanglenn16@outlook.com>
2024-12-29 16:44:08 -0600

    fix: use custom message dispatcher that allows function passing

--- project/parttofe/partto/src/shared/runStateMessage.ts
+++ project/parttofe/partto/src/shared/runStateMessage.ts
@@ -1,53 +1,30 @@
 import { RunState } from "../api/sharedschemas";
 
-let LocalMessageChannel;
-try {
-  // eslint-disable-next-line
-  LocalMessageChannel = MessageChannel;
-} catch {
-  // likely in node here (unit tests)
-}
-
-try {
-  // eslint-disable-next-line
-  LocalMessageChannel = require("worker_threads").MessageChannel;
-} catch {
-  //   likely in a browser here
-}
-
-const runStateChannel = new LocalMessageChannel();
-
-type Listener = {
-  unparsed: (message: MessageEvent<string>) => void;
-  parsed: (runState: RunState) => void;
-};
-
-let listeners: Array<Listener> = [];
+let listeners: Array<(runState: RunState) => void> = [];
 
 let lastRunState: RunState;
 
-export function addRunStateListener(listener: (runState: RunState) => void) {
-  const unparsedListener = (unparsed: MessageEvent<string>) => {
-    listener(JSON.parse(unparsed.data));
-  };
-  if (listeners.some(({ unparsed }) => unparsedListener === unparsed)) {
+export function addRunStateListener(newListener: (runState: RunState) => void) {
+  if (listeners.some((listener) => listener === newListener)) {
     return;
   }
-  lastRunState && listener(lastRunState);
-  listeners.push({ unparsed: unparsedListener, parsed: listener });
-  runStateChannel.port1.addEventListener("message", unparsedListener);
+  lastRunState && newListener(lastRunState);
+  listeners.push(newListener);
 }
 
-export function removeRunStateListener(listener: (runState: RunState) => void) {
-  const removable = listeners.find(({ parsed }) => parsed === listener);
+export function removeRunStateListener(
+  oldListener: (runState: RunState) => void,
+) {
+  const removable = listeners.find((listener) => oldListener === listener);
   if (!removable) {
     return;
   }
-  runStateChannel.port1.removeEventListener("message", removable.unparsed);
-  listeners = listeners.filter(({ parsed }) => parsed === listener);
+  listeners = listeners.filter((removable) => removable !== oldListener);
 }
 
 export function broadcastRunState(runState: RunState) {
   lastRunState = runState;
-  runStateChannel.port2.postMessage(JSON.stringify(runState));
+  for (const listener of listeners) {
+    listener(lastRunState);
+  }
 }
