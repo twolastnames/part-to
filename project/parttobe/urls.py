d121d3a73d6477a2318bc8e6715354f6fb99360a
Alan Glenn <alanglenn16@outlook.com>
2024-12-31 12:21:05 -0600

    style: remove commented out dead code from early on

--- project/parttobe/urls.py
+++ project/parttobe/urls.py
@@ -8,20 +8,6 @@ def get_documented_paths():
     ids = set([endpoints.OperationId(id).name() for id in endpoints.operations.keys()])
     return [validation.path_from_operation_id(id) for id in ids]
 
-
-# urlpatterns = [
-#    path(
-#        "hello-world/",
-#        views.hello_world,
-#        name="hello_world",
-#    ),
-#    path(
-#        "ingredients/",
-#        views.ingredients,
-#        name="ingredients",
-#    ),
-#    path("tools/", views.tools, name="tools"),
-# ]
 urlpatterns = []
 
 urlpatterns.extend(get_documented_paths())
