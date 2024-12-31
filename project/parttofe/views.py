219ac912ff290ab54d8b67e57c226d3de9cf5e24
Alan Glenn <alanglenn16@outlook.com>
2024-12-31 08:53:44 -0600

    fix: have media assets in demo environment

--- project/parttofe/views.py
+++ project/parttofe/views.py
@@ -4,19 +4,17 @@ import os
 import re
 import shutil
 
-file_types = ['css', 'js', 'media']
+file_types = ["css", "js", "media"]
 
 self_directory = os.path.dirname(os.path.abspath(__file__))
+
 
 def get_path(type):
     return os.path.join(self_directory, "partto", "build", "static", type)
 
 
-
 try:
-    raw_files = { type: os.listdir(get_path(type))
-        for type in file_types
-    }
+    raw_files = {type: os.listdir(get_path(type)) for type in file_types}
 except:
     print("could not find a type directory")
     files = []
@@ -24,10 +22,7 @@ except:
 
 try:
     shaed_files = {
-        type: [
-            "/static/static/{}/{}".format(type, filename)
-            for filename in filenames
-        ]
+        type: ["/static/static/{}/{}".format(type, filename) for filename in filenames]
         for type, filenames in raw_files.items()
     }
 except KeyError:
@@ -41,19 +36,21 @@ import json
 media_path = os.path.join(self_directory, "partto", "build", "media")
 
 if not os.path.isdir(media_path):
-   os.makedirs(media_path)
+    os.makedirs(media_path)
 
-for file in os.listdir(get_path('media')):
-    shutil.copy(os.path.join(self_directory, "partto", "build", "static", "media",file),
-    os.path.join(self_directory, "partto", "build", "media")
+for file in os.listdir(get_path("media")):
+    shutil.copy(
+        os.path.join(self_directory, "partto", "build", "static", "media", file),
+        os.path.join(self_directory, "partto", "build", "media"),
     )
+
 
 def index(*args, **kargs):
     return render(
         args[0],
         "entry/index.html",
         context={
-            "js_files" : shaed_files['js'],
-            "css_files" : shaed_files['css'],
+            "js_files": shaed_files["js"],
+            "css_files": shaed_files["css"],
         },
     )
