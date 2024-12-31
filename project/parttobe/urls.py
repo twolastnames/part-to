2ec2afaa9cf9b1dcceeeaa443ac9290efeb5e84c
Alan Glenn <alanglenn16@outlook.com>
2024-12-31 15:27:44 -0600

    style: obey black spacing rules

--- project/parttobe/urls.py
+++ project/parttobe/urls.py
@@ -8,6 +8,7 @@ def get_documented_paths():
     ids = set([endpoints.OperationId(id).name() for id in endpoints.operations.keys()])
     return [marshaling.path_from_operation_id(id) for id in ids]
 
+
 urlpatterns = []
 
 urlpatterns.extend(get_documented_paths())
