b4fee37edea17c30e9e54063152165f1d189056a
Alan Glenn <alanglenn16@outlook.com>
2025-01-19 15:10:46 -0600

    feat: have accordions default open

--- project/parttofe/partto/src/components/Accordion/Accordion.tsx
+++ project/parttofe/partto/src/components/Accordion/Accordion.tsx
@@ -5,7 +5,7 @@ import { AccordionProps } from "./AccordionTypes";
 
 export function Accordion({ summary, children }: AccordionProps) {
   return (
-    <details className={classes.accordion} data-testid="Accordion">
+    <details className={classes.accordion} open={true} data-testid="Accordion">
       <summary>{summary}</summary>
       <div className={classes.body}>{children}</div>
     </details>
